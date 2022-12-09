68050ad37d9457597fbaa966eaa30833d78e5987
chris <chrisvuong229@gmail.com>
2022-12-08 23:28:44 -0800

    added error handling and fixed console log

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const rl = readline.createInterface(
   process.stdin, process.stdout);
 
 // User enters a float.
-// TODO add error handling and not print undefined.
+// TODO: add error handling and not print undefined. add
 function input() {
-  rl.question('Please enter a number:', (number) => {
-    const RESPONSE = parseFloat(number);
-    const quarters = divideQuarter.operation(RESPONSE);
-    const dimes = divideDime.operation(quarters.balance);
-    const nickels = divideNickel.operation(dimes.balance);
-    const pennies = nickels.balance;
-    console.log('There are...')
-    console.log(`Quarters: ${quarters.numberOfQuarters}, Dimes: ${dimes.numberOfDimes}, Nickels: ${nickels.numberOfNickels}
-    and Pennies: ${pennies}`) 
+  rl.question('Please enter a dollar amount:', (value) => {
+    if (Number(value)) {
+      const RESPONSE = parseFloat(value) * 100;
+      const quarters = divideQuarter.operation(RESPONSE);
+      const dimes = divideDime.operation(quarters.balance);
+      const nickels = divideNickel.operation(dimes.balance);
+      const pennies = nickels.balance;
+      console.log('There are...')
+      console.log(
+        `\nQuarters: ${quarters.numberOfQuarters ? quarters.numberOfQuarters : 0}
+        \nDimes: ${dimes.numberOfDimes ? dimes.numberOfDimes : 0}
+        \nNickels: ${nickels.numberOfNickels ? nickels.numberOfNickels : 0}
+        \nPennies: ${pennies ? pennies : 0}`)
+        process.exit()
+    } else {
+      console.log('Please enter a numeric value!');
+      input()
+    }
   })
 }
 
